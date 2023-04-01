77974c2be4202439b57865a676c828c87f5c6b4e
Khushal-ag <starkhush5@gmail.com>
2023-04-01 23:27:34 +0530

    remove unwanted comments

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-    const express = require("express"); // npm i express
-    const path = require("path"); // npm i path
-    // require('dotenv').config() //npm i dotenv
+    const express = require("express");
+    const path = require("path");
     const app = express();
 
 
-    app.set('view engine', 'ejs'); // npm i ejs
-    app.use(express.urlencoded({ extended: true })) //npm i body-parser
+    app.set('view engine', 'ejs');
+    app.use(express.urlencoded({ extended: true }))
     app.use(express.static(path.join(__dirname,'public')))
     app.set('views', path.join(__dirname, '/views'));
 
-    app.get('/', (req, res) => { 
+    app.get('/', (req, res) => {
     res.render('TripHive Home');
     })
 
