0ce9da8e8420ed19033c0e12cc790f4fd3ab31d2
Khushal-ag <starkhush5@gmail.com>
2023-04-02 23:51:41 +0530

    error handling (error class and handled async exceptions)

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const mongo = require('mongoose');
 const Hotel = require('./models/hotel');
 const Review = require('./models/review');
 
+const ExpressError = require('./utils/expressError');
+const CatchAsync = require('./utils/catchAsync');
+
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'));
 
@@ -31,49 +34,49 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/hotel', async (req, res) => {
+app.get('/hotel', CatchAsync(async (req, res) => {
     const hotels = await Hotel.find({}).sort({ title: 1 })
     res.render('hotel/index', { hotels });
-})
+}))
 
 app.get('/hotel/new', (req, res) => {
     res.render('hotel/new')
 })
 
-app.get('/hotel/:id', async (req, res) => {
-    const { id } = req.params
-    const hotel = await Hotel.findById(id).populate('reviews')
-    res.render('hotel/show', { hotel })
-})
-
-app.get('/hotel/:id/edit', async (req, res) => {
-    const { id } = req.params
-    const hotel = await Hotel.findById(id)
-    res.render('hotel/edit', { hotel })
-})
-
-app.post('/hotel', async (req, res) => {
+app.post('/hotel', CatchAsync(async (req, res) => {
     const hoteldata = new Hotel(req.body.hotel)
     console.log(hoteldata)
     await hoteldata.save()
     res.redirect(`/hotel/${hoteldata._id}`)
-})
+}))
 
-app.put('/hotel/:id',async (req, res) => {
+app.get('/hotel/:id', CatchAsync(async (req, res) => {
     const { id } = req.params
-    const hoteldata = req.body.hotel
-    const updated = await Hotel.findByIdAndUpdate(id, hoteldata, { runValidators: true, new: true })
-    res.redirect(`/hotel/${id}`)
-})
+    const hotel = await Hotel.findById(id).populate('reviews')
+    res.render('hotel/show', { hotel })
+}))
 
-app.delete('/hotel/:id', async (req, res) => {
+app.delete('/hotel/:id', CatchAsync(async (req, res) => {
     const { id } = req.params
     console.log(id)
     const deleted = await Hotel.findByIdAndDelete(id)
     res.redirect('/hotel')
-})
+}))
 
-app.post('/hotel/:id/reviews', async (req, res) => {
+app.get('/hotel/:id/edit', CatchAsync(async (req, res) => {
+    const { id } = req.params
+    const hotel = await Hotel.findById(id)
+    res.render('hotel/edit', { hotel })
+}))
+
+app.put('/hotel/:id', CatchAsync(async (req, res) => {
+    const { id } = req.params
+    const hoteldata = req.body.hotel
+    const updated = await Hotel.findByIdAndUpdate(id, hoteldata, { runValidators: true, new: true })
+    res.redirect(`/hotel/${id}`)
+}))
+
+app.post('/hotel/:id/reviews', CatchAsync(async (req, res) => {
     const { id } = req.params
     const hotel = await Hotel.findById(id)
     const review = new Review(req.body.review)
@@ -81,11 +84,21 @@ app.post('/hotel/:id/reviews', async (req, res) => {
     await review.save()
     await hotel.save()
     res.redirect(`/hotel/${hotel._id}`)
-})
+}))
 
-app.delete('/hotel/:id/reviews/:reviewId',async (req, res) => {
+app.delete('/hotel/:id/reviews/:reviewId', CatchAsync(async (req, res) => {
     const { id, reviewId } = req.params
     await Hotel.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
     await Review.findByIdAndDelete(reviewId)
     res.redirect(`/hotel/${id}`)
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = 'Something went wrong'
+    res.status(statusCode).render('error', { err })
 })
