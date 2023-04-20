fd86984061965802f4846c544192fcd95ed04542
Khushal Agarwal <starkhush5@gmail.com>
2023-04-21 01:03:58 +0530

    shift db and sessions to atlas

--- app.js
+++ app.js
@@ -3,36 +3,39 @@ const express = require("express");
 const path = require("path");
 const app = express();
 require('dotenv').config();
-const ejsmate = require('ejs-mate')
+const ejsmate = require('ejs-mate');
 const mongo = require('mongoose');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
-const User = require('./models/user')
-const userRoutes = require('./routes/userRoutes')
-const hotelRoutes = require('./routes/hotelRoutes')
-const reviewRoutes = require('./routes/reviewRoutes')
-const shopRoutes = require('./routes/shopRoutes')
+const MongoStore = require('connect-mongo');
+
+const User = require('./models/user');
+const userRoutes = require('./routes/userRoutes');
+const hotelRoutes = require('./routes/hotelRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const shopRoutes = require('./routes/shopRoutes');
+
+const dbUrl = process.env.MONGO_URI || 'mongodb://localhost:27017/triphive';
 
 //Express configuration
 app.set('view engine', 'ejs');
-app.engine('ejs', ejsmate)
+app.engine('ejs', ejsmate);
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
     replaceWith: '_'
-}))
-
+}));
 
 //MongoDB Connection
-mongo.set('strictQuery', false)
-mongo.connect(process.env.Mongo_URI)
+mongo.set('strictQuery', false);
+mongo.connect(dbUrl);
 
 const db = mongo.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -40,44 +43,58 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+//Session Store
+const secret = process.env.SESSION_SECRET || 'ThisIsATopSecret';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    ttl: 14 * 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e);
+});
+
 //Session
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: process.env.SESSION_SECRET,
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
         // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
 
-app.use(session(sessionConfig))
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //Global var Middleware
 app.use((req, res, next) => {
-    if (!['/login', '/register', '/', '/favicon.ico'].includes(req.originalUrl)) {
-        req.session.returnTo = req.originalUrl
+    if (!['/login', '/register', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
     }
-    res.locals.currentUser = req.user
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
-    next()
-})
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Routes
-app.use('/', userRoutes)
-app.use('/hotel', hotelRoutes)
-app.use('/hotel/:id/reviews', reviewRoutes)
-app.use('/shop', shopRoutes)
+app.use('/', userRoutes);
+app.use('/hotel', hotelRoutes);
+app.use('/hotel/:id/reviews', reviewRoutes);
+app.use('/shop', shopRoutes);
 
 //Endppoints
 app.get('/', (req, res) => {
@@ -85,15 +102,15 @@ app.get('/', (req, res) => {
 })
 
 app.all('*', (req, res, next) => {
-    next(res.status(404).render('pageNotFound'))
-})
+    next(res.status(404).render('pageNotFound'));
+});
 
 //Error Handler
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err
-    if (!err.message) err.message = 'Something went wrong'
-    res.status(statusCode).render('error', { err })
-})
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong';
+    res.status(statusCode).render('error', { err });
+});
 
 //Server
 app.listen(process.env.PORT, () => {
--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -12,6 +12,6 @@ map.addControl(new mapboxgl.NavigationControl());
 new mapboxgl.Marker().setLngLat(hotel.geometry.coordinates).setPopup(
     new mapboxgl.Popup({ offset: 25 })
         .setHTML(
-            `<div class='mt-3'><h4>${hotel.title}</h4><a href="https://www.google.com/maps/place/${hotel.location}" class='text-decoration-none text-secondary' target="_blank">${hotel.location}</a></div>`
+            `<div class='mt-3 text-dark'><h4>${hotel.title}</h4><a href="https://www.google.com/maps/place/${hotel.location}" class='text-decoration-none text-secondary' target="_blank">${hotel.location}</a></div>`
         )
 ).addTo(map)
