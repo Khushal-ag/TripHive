10cc0a605546caa60f7c100123b9105952de184b
Khushal Agarwal <starkhush5@gmail.com>
2023-04-22 15:53:46 +0530

    home and form style tweaks

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     if (!['/login', '/register', '/'].includes(req.originalUrl)) {
         req.session.returnTo = req.originalUrl;
-    }
+    };
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
